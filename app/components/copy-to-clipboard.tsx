5db4deead5331b7b13c3c96c2a959c2c1695442a
Emanuel Peire <emapeire@outlook.com>
2024-03-28 15:59:49 -0300

    Fix copy to clipboard functionallity

--- app/components/copy-to-clipboard.tsx
+++ app/components/copy-to-clipboard.tsx
@@ -1,17 +1,22 @@
 "use client";
 
+import { useState } from "react";
+import CheckIcon from "./icons/check-icon";
 import ClipboardIcon from "./icons/clipboard-icon";
 
 export default function CopyToClipboard({ code }: { code: string }) {
+  const [copied, setCopied] = useState(false);
+
   const copyToClipboard = async () => {
     try {
       await navigator.clipboard.writeText(code);
+      setCopied(true);
       console.log("Copied to clipboard");
     } catch (error) {
       console.error("Error copying to clipboard", error);
     } finally {
       setTimeout(() => {
-        // do something
+        setCopied(false);
       }, 2000);
     }
   };
@@ -19,9 +24,9 @@ export default function CopyToClipboard({ code }: { code: string }) {
   return (
     <button
       onClick={copyToClipboard}
-      className="text-[#646464] hover:text-[#d8d8d8] transition-colors duration-200 ease-in-out"
+      className="text-[#646464] hover:text-[#c4c4c4] transition-colors duration-200 ease-in-out"
     >
-      <ClipboardIcon />
+      {copied ? <CheckIcon /> : <ClipboardIcon />}
     </button>
   );
 }
